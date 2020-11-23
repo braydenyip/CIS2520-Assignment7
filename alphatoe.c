30c429841705a82a6cde3a749e17f9332fcd1714
Brayden Yip <yipb@uoguelph.ca>
2020-11-22 21:54:57 -0500

    Updated alphatoe.c -- see Courselink

--- alphatoe.c
+++ alphatoe.c
@@ -1,59 +1,66 @@
+/*
+ * Fixed version of alphatoe.c
+ * Credit: Samuel Guilbeault
+ */
+
 #include "ttt.h"
 #include <stdio.h>
 #include <ctype.h>
 
-int main( int argc, char **argv )
+#define MAX_INPUT 512
+
+int main(int argc, char **argv)
 {
   char human;
+  char input[MAX_INPUT];
 
   init_boards();
   init_board(START_BOARD);
   join_graph(START_BOARD);
   compute_score();
 
-  
-  printf( "X or O? " );
-  fflush( stdin ); // clear any extra characters in the input stream
-  human = getc( stdin );
+  printf("X or O? ");
+  fgets(input, MAX_INPUT, stdin);
+  human = input[0];
 
   int board = 0;
-  
-  while ( isalpha( htable[board].turn ) )	// while no winner
+
+  while (isalpha(htable[board].turn)) // while no winner
   {
-    printf( "%d\n", board );
-    if (human==htable[board].turn)
+    printf("%d\n", board);
+    if (human == htable[board].turn)
     {
       int next_board;
       do
       {
-	char movec;
-	int movei;
-        print_node( htable[board] );
-	printf( "Your move [0-8]: " );
-        fflush( stdin ); // clear any extra characters in the input stream
-	movec = getc( stdin );
-	movei = (int)(movec-'0');
-	next_board = htable[board].move[movei];
-	if (next_board==-1)
-	{
-	  printf( "Illegal move, try again!\n" );
-	}
-      } while (next_board==-1);
+        char movec;
+        int movei;
+        print_node(htable[board]);
+        printf("Your move [0-8]: ");
+        fgets(input, MAX_INPUT, stdin);
+        movec = input[0];
+        movei = (int)(movec - '0');
+        next_board = htable[board].move[movei];
+        if (next_board == -1)
+        {
+          printf("Illegal move, try again!\n");
+        }
+      } while (next_board == -1);
       board = next_board;
-    } 
-    else // computer's turn
+    }
+    else // computers turn
     {
       board = htable[board].move[best_move(board)];
-    } // computer's turn
-  } // while loop
+    } // computers turn
+  }   // while loop
 
-  print_node( htable[board] );
-  printf( "Game over, Player 1!\n" );
+  print_node(htable[board]);
+  printf("Game over, Player 1!\n");
 
-  if ( htable[board].winner==human )
-    printf( "You won!\n" );
-  else if ( htable[board].winner=='-' )
-    printf( "The game was a tie!\n" );
+  if (htable[board].winner == human)
+    printf("You won!\n");
+  else if (htable[board].winner == '-')
+    printf("The game was a tie!\n");
   else
-    printf( "You lost!\n" );
+    printf("You lost!\n");
 }
