c545b46c87edea21ed7d70dda1ea5bb714a79158
Brayden Yip <yipb@uoguelph.ca>
2020-11-22 22:34:59 -0500

    Added count and show functions

--- count_nodes.c
+++ count_nodes.c
@@ -1,2 +1,19 @@
 #include "ttt.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+  init_boards(); // set all the init to -1
+  init_board(START_BOARD); // initalize with the START_BOARD
+  join_graph(START_BOARD); // make all the boards based on START_BOARD
+  int count = 0;
+  // brute force methods -- i'm gonna probably make this a recursive search
+  int i = 0;
+  for (i = 0; i < HSIZE; i++) {
+    if (htable[i]->init == 1) {
+      count++;
+    }
+  }
+  return count;
+}
--- show_node.c
+++ show_node.c
@@ -1,2 +1,17 @@
 #include "ttt.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+  init_boards(); // set all the init to -1
+  init_board(START_BOARD); // initalize with the START_BOARD
+  join_graph(START_BOARD); // make all the boards based on START_BOARD
+  int i = 0;
+  int board_num = 0; // stores the board number from argv[]  
+  for (i = 1; i < argc; i++) { // iterate thru args
+    board_num = atoi(argv[i]);
+    print_node(htable[board_num]);
+  }
+  return 0;
+}
